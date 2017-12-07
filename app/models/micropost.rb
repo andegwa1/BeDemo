8ac51ca136a9b84a6ed850020b785c8868c5b654
Al N <andegwa1@student.gsu.edu>
2017-12-07 14:55:03 +0000

    More video changes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,7 +8,7 @@ class Micropost < ApplicationRecord
 
   private
 
-    # Validates the size of an uploaded picture.
+    # Validates the size of an uploaded video.
     def video_size
       if video.size > 15.megabytes
         errors.add(:video, "should be less than 15MB")
