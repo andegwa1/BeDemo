570ef2640abeaf4a3d0eabdbf05e4e93880c3f89
Al N <andegwa1@student.gsu.edu>
2017-12-07 05:25:11 +0000

    Video upload

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content, :video)
     end
 
     def correct_user
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,17 +1,17 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
+  mount_uploader :video, VideoUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate  :picture_size
+  validate  :video_size
 
   private
 
     # Validates the size of an uploaded picture.
-    def picture_size
-      if picture.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
+    def video_size
+      if video.size > 15.megabytes
+        errors.add(:video, "should be less than 15MB")
       end
     end
 end
